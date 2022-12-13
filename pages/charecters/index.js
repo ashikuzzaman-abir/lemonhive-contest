92d384a671c98bb3b73489f7d349750c47172039
Ashikuzzaman Abir <ashikuzzamanabir@outlook.com>
2022-12-13 23:48:10 +0600

    update

--- pages/charecters/index.js
+++ pages/charecters/index.js
@@ -20,7 +20,7 @@ function index() {
 			<Pagination />
 			<div className='sm:h-[40px] md:h-[96px]'></div>
 			{/* background */}
-			<div className='absolute top-0 left-[50%] sm:w-[1300px] md:w-full h-full'>
+			<div className='absolute top-0 left-[50%] sm:w-[1300px] md:w-screen h-full'>
 				<Image src={background} fill alt='background' />
 			</div>
 			<div className='sm:hidden md:block absolute bottom-0 left-0 w-full h-[775px]'>
