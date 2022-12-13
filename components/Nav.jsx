f3ebfb1b5c0ca449321cd02fa99c39ab5b31fb71
Ashikuzzaman Abir <ashikuzzamanabir@outlook.com>
2022-12-13 23:27:04 +0600

    update

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 function Nav() {
-  return (
-		<div className=''>
-			<div className='w-[100vw] flex justify-center items-center  pt-8 h-[60px]'>
-				<div className='relative sm:w-[133px] sm:h-[28px] md:w-[227px] md:h-[48px]'>
-					<Image src='/assets/Media asset/Logo.png' fill />
+	return (
+		<Link href="/">
+			<div className=''>
+				<div className='w-[100vw] relative z-[50] flex justify-center items-center  pt-8 h-[60px]'>
+					<div className='relative sm:w-[133px] sm:h-[28px] md:w-[227px] md:h-[48px]'>
+						<Image src='/assets/Media asset/Logo.png' fill />
+					</div>
 				</div>
 			</div>
-		</div>
+		</Link>
 	);
 }
 
-export default Nav
+export default Nav;
